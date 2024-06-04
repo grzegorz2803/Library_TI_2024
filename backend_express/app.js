67ada533b85c6eb8fb908615ea7491feb8c602dc
Grzegorz Listwan <listwan94@gmail.com>
2024-06-04 14:36:26 +0200

    Connect with database

--- backend_express/app.js
+++ backend_express/app.js
@@ -1,41 +1,33 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// app.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const sequelize = require('./config/database');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+app.use(bodyParser.json());
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// Importowanie modeli
+const User = require('./models/User');
+const Book = require('./models/Book');
+const Loan = require('./models/Loan');
+const Reservation = require('./models/Reservation');
+const Log = require('./models/Log');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Synchronizacja modeli z bazą danych
+sequelize.authenticate()
+    .then(() => {
+      console.log('Connection has been established successfully.');
+      return sequelize.sync();
+    })
+    .then(() => {
+      console.log('Database & tables created!');
+    })
+    .catch(err => {
+      console.error('Unable to connect to the database:', err);
+    });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
