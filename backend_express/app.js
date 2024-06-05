2a1f3fc97887bd422b8adc9dac44d139c8c0928a
Grzegorz Listwan <listwan94@gmail.com>
2024-06-05 20:47:16 +0200

    change port 3001

--- backend_express/app.js
+++ backend_express/app.js
@@ -77,7 +77,7 @@ sequelize.authenticate()
         console.error('Unable to connect to the database:', err);
     });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
