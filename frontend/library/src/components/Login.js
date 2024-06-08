70643c40dc7b3f995d506ec624ed9267134437ff
Sylwia Jaworska <se.jaworska@gmail.com>
2024-06-08 09:22:15 +0200

    Cookies

--- frontend/library/src/components/Login.js
+++ frontend/library/src/components/Login.js
@@ -1,12 +1,29 @@
-import React, { useState } from 'react';
+import React, {useState, useEffect} from 'react';
+import Cookies from 'js-cookie';
 
-function Login({ login }) {
+function Login({login}) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [rememberMe, setRememberMe] = useState(false);
     const [error, setError] = useState('');
+
+    // Sprawdzenie, czy istnieją zapisane dane logowania w cookies podczas ładowania komponentu
+    useEffect(() => {
+        const savedUsername = Cookies.get('username');
+        if (savedUsername) {
+            setUsername(savedUsername);
+            setRememberMe(true);
+        }
+    }, []);
 
     const handleLogin = async (e) => {
         e.preventDefault();
+
+/*        if (rememberMe) {
+            Cookies.set('username', username, {expires: 365});
+        } else {
+            Cookies.remove('username');
+        }*/
 
         try {
             const response = await fetch('http://localhost:3001/users/login', {
@@ -14,12 +31,20 @@ function Login({ login }) {
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ login: username, password: password })
+                body: JSON.stringify({login: username, password: password})
             });
 
             const data = await response.json();
 
+
             if (response.ok) {
+                // Zapamiętywanie nazwy użytkownika, jeśli opcja "Remember Me" jest zaznaczona
+                if (rememberMe) {
+                    Cookies.set('username', username, {expires: 365});
+                } else {
+                    Cookies.remove('username');
+                }
+
                 // Ustawienie stanu użytkownika na zalogowanego
                 login(data);
                 console.log(data);
@@ -52,6 +77,16 @@ function Login({ login }) {
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
+                </div>
+                <div>
+                    <label>
+                        <input
+                            type="checkbox"
+                            checked={rememberMe}
+                            onChange={(e) => setRememberMe(e.target.checked)}
+                        />
+                        Remember Me
+                    </label>
                 </div>
                 <button type="submit">Log in</button>
                 {error && <p className="error">{error}</p>}
